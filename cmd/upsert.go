79108b6ece4106e5737467494b397386458ce2d0
johnnychang25678 <zjps3407@gmail.com>
2022-10-07 17:19:27 +0800

    feat: add upsert cmd and common error

---
+++ cmd/upsert.go
@@ -0,0 +1,108 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/johnnychang25678/my-words-app/common"
+	"github.com/johnnychang25678/my-words-app/repository"
+	"github.com/spf13/cobra"
+)
+
+// upsertCmd represents the upsert command
+var upsertCmd = &cobra.Command{
+	Use:   "upsert",
+	Short: "Insert or update word(s) to the database.",
+	Long: `
+Insert new words, or update existed words' defintion in database.
+You can either upsert one word with terminal or multiple words at once with .csv file.
+* If use --file flag, make sure it's a csv file and the first column is word, second column is definition
+
+Support below two usages: 
+- upsert [word] [definition]
+- upsert --file filename`,
+
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) == 0 {
+			fileName, _ := cmd.Flags().GetString("file")
+			file, err := os.OpenFile(fileName, os.O_RDONLY, 0644)
+			if err != nil {
+				appErr := common.AppError{ErrorCode: common.OpenFileError, Err: err}
+				appErr.PrintAppError()
+				return
+			}
+			words, appErr := getWordsFromFile(file)
+			if appErr != nil {
+				appErr.PrintAppError()
+				return
+			}
+			// bulk upsert to db
+			if err := repository.WordRepo.BulkUpsert(words); err != nil {
+				appErr := common.AppError{ErrorCode: common.DbError, Err: err}
+				appErr.PrintAppError()
+			}
+		} else if len(args) != 2 {
+			printUpsertErr("Please provide two args [word] [definition]")
+		} else {
+			// upsert one word to db
+			word, definition := args[0], args[1]
+			if word == "" {
+				printUpsertErr("word cannot be empty")
+				return
+			}
+			if definition == "" {
+				printUpsertErr("definition cannot be empty")
+				return
+			}
+			if err := repository.WordRepo.Upsert(word, definition); err != nil {
+				appErr := common.AppError{ErrorCode: common.DbError, Err: err}
+				appErr.PrintAppError()
+			}
+		}
+	},
+}
+
+func printUpsertErr(errMessage string) {
+	err := fmt.Errorf(errMessage)
+	appErr := common.AppError{ErrorCode: common.UpsertError, Err: err}
+	appErr.PrintAppError()
+}
+
+func getWordsFromFile(file *os.File) ([]repository.Word, *common.AppError) {
+	defer file.Close()
+	csvReader := csv.NewReader(file)
+	var words []repository.Word
+	for {
+		record, err := csvReader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return nil, &common.AppError{ErrorCode: common.ReadCsvError, Err: err}
+		}
+		if len(record) != 2 {
+			err = fmt.Errorf("csv format is incorrect. Please make sure there are exact two columns (separate with comma) for every row")
+			return nil, &common.AppError{ErrorCode: common.ReadCsvError, Err: err}
+		}
+		words = append(words, repository.Word{Word: record[0], Definition: record[1]})
+	}
+	if len(words) == 0 {
+		err := fmt.Errorf("There are no words in the file")
+		return nil, &common.AppError{ErrorCode: common.ReadCsvError, Err: err}
+	}
+	return words, nil
+}
+
+func init() {
+	rootCmd.AddCommand(upsertCmd)
+
+	// name: use with 2 dashes: e.g.: --toggle
+	// shorthand: use with only one dash, e.g.: -t
+	upsertCmd.Flags().StringP("file", "f", "", "File")
+}
---
+++ common/error.go
@@ -0,0 +1,23 @@
+package common
+
+import (
+	"fmt"
+)
+
+type ErrorCode string
+
+const (
+	UpsertError   ErrorCode = "UPSERT_ERROR"
+	OpenFileError ErrorCode = "OPEN_FILE_ERROR"
+	ReadCsvError  ErrorCode = "READ_CSV_ERROR"
+	DbError       ErrorCode = "DB_ERROR"
+)
+
+type AppError struct {
+	ErrorCode ErrorCode
+	Err       error
+}
+
+func (a AppError) PrintAppError() {
+	fmt.Println("**ERROR**", fmt.Sprintf("code: %s, message: %s", a.ErrorCode, a.Err.Error()))
+}
