5c329594f9f551bbcbec8725203efdde171185ff
johnnychang25678 <zjps3407@gmail.com>
2024-01-01 23:15:01 -0800

    feat: move cli to cli_app and create new module my_words_app_2.0

---
+++ cli_app/go.mod
@@ -0,0 +1,3 @@
+module github.com/johnnychang25678/my-words-app
+
+go 1.18
---
+++ cli_app/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/johnnychang25678/my_words_app_2.0/db"
+
+	"github.com/johnnychang25678/my-words-app/cmd"
+	"github.com/johnnychang25678/my-words-app/repository"
+	"github.com/joho/godotenv"
+)
+
+func initRepos(db *sql.DB) {
+	repository.InitWordRepo(db)
+	repository.InitTestRepo(db)
+}
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		if os.IsNotExist(err) {
+			if _, err := os.Create(".env"); err != nil {
+				log.Fatal(err)
+			}
+			fmt.Println(".env file not exist. It's created now, please try again. Check README to see .env settings.")
+			os.Exit(0)
+		}
+		log.Fatal(err)
+	}
+	database, err := db.ConnectToDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	initRepos(database)
+	cmd.Execute()
+}
